88b1e928b8a2f1a700d6d7685d085f365177cbc4
Mark Bekhet <mark.bekhet@ericsson.com>
2021-04-16 09:25:50 -0400

    Introduce an optional configuration to define a regex for naming rules
    Before this change, this plugin enforced a rule that a project cannot
    contain spaces in its name. Therefore, projects could have a name with
    any set of characters while not including a space in their names.
    However, due to IRI (not URI) and backward compatibility, the non-ASCII
    characters may be represented in a way that can cause issues [1].
    
    This change introduces a configurable parameter that can be set to
    define a regex that is then enforced upon project creation to ensure
    backward compatibility. If not defined, the default behavior, to check
    that projects don't contain spaces, applies.
    
    If the regex is defined, it will apply if it accepts the character /,
    else it will get ignored to not affect the functionality of the
    plugin. The plugin rejects projects with spaces in their names even if
    the regex includes the space.
    
    [1] https://en.wikipedia.org/wiki/Internationalized_Resource_Identifier
    
    Bug: Issue 14297
    Change-Id: Ia21c9834f41c4d5289f56aacd357c5de09f4521f

---
+++ src/main/java/com/ericsson/gerrit/plugins/projectgroupstructure/Configuration.java
@@ -0,0 +1,56 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License"),
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.ericsson.gerrit.plugins.projectgroupstructure;
+
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import org.eclipse.jgit.lib.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+class Configuration {
+  private static final Logger log = LoggerFactory.getLogger(Configuration.class);
+  private static final String NAME_REGEX = "nameRegex";
+  private static final String DEFAULT_NAME_REGEX_MESSAGE =
+      "The value of the regex is not set or it is invalid because it doesn't accept /."
+          + " Using default wildcard regex.";
+  private static final String DEFAULT_REGEX_VALUE = ".+";
+
+  private String regexNameFilter;
+  private String pluginName;
+
+  @Inject
+  Configuration(PluginConfigFactory pluginConfigFactory, @PluginName String pluginName) {
+    Config config = pluginConfigFactory.getGlobalPluginConfig(pluginName);
+    regexNameFilter = config.getString(pluginName, null, NAME_REGEX);
+    if (regexNameFilter == null || (regexNameFilter != null && !"/".matches(regexNameFilter))) {
+      regexNameFilter = DEFAULT_REGEX_VALUE;
+      log.warn(DEFAULT_NAME_REGEX_MESSAGE);
+    }
+
+    this.pluginName = pluginName;
+  }
+
+  public String getRegexNameFilter() {
+    return regexNameFilter;
+  }
+
+  public String getPluginName() {
+    return pluginName;
+  }
+}
